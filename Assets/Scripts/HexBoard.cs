316e485c66ad07ecce35492f71f4497e3166c5fd
Steve <Steve5805@gmail.com>
2024-01-17 20:03:01 -0500

    Adds hex board instantiation

--- Assets/Scripts/HexBoard.cs
+++ Assets/Scripts/HexBoard.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class HexBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static HexBoard Active;
+
+    public Hex HexPrefab;
+
+    public Dictionary<Vector3Int, Hex> Hexes = new Dictionary<Vector3Int, Hex>();
+
+    public int BoardRadius; // The radius of the board (in hexes)
+
+    private float hexWidth; // Calculated width of a hex tile
+    private float hexHeight; // Calculated height of a hex tile
+    private const float hexRadius = 1f; // Radius of a hex tile
+
+
+    /// <summary>
+    /// Sets active board, on initialization or after script recompilation. 
+    /// </summary>
+    void OnEnable()
     {
-        
+        Active = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        GenerateHexBoard();
+    }
+
+    void GenerateHexBoard()
+    {
+
+        hexWidth = 2 * hexRadius;
+        hexHeight = Mathf.Sqrt(3) * hexRadius;
+
+        for (int q = -BoardRadius; q <= BoardRadius; q++)
+        {
+            int r1 = Mathf.Max(-BoardRadius, -q - BoardRadius);
+            int r2 = Mathf.Min(BoardRadius, -q + BoardRadius);
+            for (int r = r1; r <= r2; r++)
+            {
+                // Instantiate a Hex
+                Hex hex = Instantiate(HexPrefab);
+                // Calculate the position for this Hex
+                Vector2 position = HexToPosition(q, r);
+                hex.transform.position = new Vector3(position.x, position.y, 0);
+                hex.name = "Hex_" + q + "_" + r;
+            }
+        }
+    }
+
+    Vector2 HexToPosition(int q, int r)
+    {
+        float x = hexWidth * (q + r / 2f);
+        float y = hexHeight * r * 0.75f; // 0.75 accounts for the vertical stacking of hexes
+        return new Vector2(x, y);
     }
 }
